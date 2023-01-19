40f8104fd0c56a8e24784dfba9ee21f00b23b981
Ben Taylor <ben.taylor2573@gmail.com>
2023-01-19 19:49:30 +0000

    init

---
+++ web-portfolio-2023/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>this is a test</p>
+    </div>
+  )
+}
+
+export default App
