4d7a031499d8c13b35a030f18b53cb416eeaf53d
Ben Taylor <ben.taylor2573@gmail.com>
2023-01-21 10:46:16 +0000

    wip mobile navbar

--- web-portfolio-2023/src/components/base/MyNav.tsx
+++ web-portfolio-2023/src/components/base/MyNav.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './MyNav.scss'
 
 export default function MyNav() {
+    const [showDropdown, setShowDropdown] = useState(false);
+
     return (
         <nav>
             <a href="/" id="title">Ben Taylor</a>
+
+            <div className="hamburger" onClick={() => setShowDropdown(!showDropdown)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
 
             <ul>
                 <li>
