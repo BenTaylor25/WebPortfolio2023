c8be7b03f8a0648750239f676d3a0defff4b59a8
Ben Taylor <ben.taylor2573@gmail.com>
2023-01-22 12:35:38 +0000

    mobile navbar

--- web-portfolio-2023/src/components/base/MyNav.tsx
+++ web-portfolio-2023/src/components/base/MyNav.tsx
@@ -8,26 +8,53 @@ export default function MyNav() {
         <nav>
             <a href="/" id="title">Ben Taylor</a>
 
-            <div className="hamburger" onClick={() => setShowDropdown(!showDropdown)}>
+            <div className="hamburger" onClick={() => setShowDropdown(show => !show)}>
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
 
-            <ul>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-                <li>
-                    <a href="/work">Work</a>
-                </li>
-                <li>
-                    <a href="/projects">Projects</a>
-                </li>
-                <li>
-                    <a href="/analyses">Analyses</a>
-                </li>
-            </ul>
+            { (!screenPortrait() || showDropdown) &&
+                <ul>
+                    <li>
+                        <a href="/about">About</a>
+                    </li>
+                    <li>
+                        <a href="/work">Work</a>
+                    </li>
+                    <li>
+                        <a href="/projects">Projects</a>
+                    </li>
+                    <li>
+                        <a href="/analyses">Analyses</a>
+                    </li>
+                </ul>
+            }
         </nav>
     );
+}
+
+function navOptions() {
+    return (
+        <ul>
+            <li>
+                <a href="/about">About</a>
+            </li>
+            <li>
+                <a href="/work">Work</a>
+            </li>
+            <li>
+                <a href="/projects">Projects</a>
+            </li>
+            <li>
+                <a href="/analyses">Analyses</a>
+            </li>
+        </ul>
+    )
+}
+
+function screenPortrait(): boolean {
+    const { innerWidth, innerHeight } = window;
+
+    return innerWidth < innerHeight;
 }
