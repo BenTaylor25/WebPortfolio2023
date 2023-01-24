3c58f59a1e620346a5003385e23ce3c7d62629c3
Ben Taylor <ben.taylor2573@gmail.com>
2023-01-24 11:44:37 +0000

    home page

--- web-portfolio-2023/src/components/pages/Home.tsx
+++ web-portfolio-2023/src/components/pages/Home.tsx
@@ -1,9 +1,13 @@
+import './Home.scss'
 
 export default function Home() {
 
     return (
-        <>
-        test
-        </>
+        <main>
+            <h1>Ben Taylor</h1>
+            <p>Computer Science Student;</p>
+            <p>Aspiring Software Engineer.</p>
+            <img src="src/assets/portrait.jpg" alt="portrait of Ben" />
+        </main>
     );
 }
