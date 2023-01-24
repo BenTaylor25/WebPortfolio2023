2f4fd7ecf7839e1a584a9937d75558ff8cf2d8b3
Ben Taylor <ben.taylor2573@gmail.com>
2023-01-24 10:58:39 +0000

    rerender on orientation switch

--- web-portfolio-2023/src/App.tsx
+++ web-portfolio-2023/src/App.tsx
@@ -1,14 +1,22 @@
+import { useMediaQuery } from "@material-ui/core"
 import MyNav from './components/base/MyNav'
 import './App.scss'
 
 function App() {
+  const test = useMediaQuery("(orientation: portrait)");
+  console.log(test);
 
   return (
     <div className="App">
-      <MyNav />
+      {<MyNav /> || test}
       <p>this is another test</p>
     </div>
   )
 }
 
 export default App
+
+/*
+  look into
+  https://usehooks-ts.com/react-hook/use-media-query
+*/
