99597c1e2053adc63d3028ed8bfafaf37eded22f
Ben Taylor <ben.taylor2573@gmail.com>
2023-01-29 10:59:10 +0000

    restructured about and wip education

--- web-portfolio-2023/src/components/pages/About.tsx
+++ web-portfolio-2023/src/components/pages/About.tsx
@@ -5,62 +5,154 @@ export default function About() {
     return (
         <main className='about'>
             <section className='bio'>
-                <h1>Bio</h1>
-                <p>
-                    Hi, my name is Ben and I'm a Computer Science student at Oxford Brookes.
-                    I love bringing ideas and designs to life through problem solving and
-                    code, and am keen to pursuit a career in Desktop-, Web-, Mobile-,
-                    or Game Development after I graduate.
-                </p>
-                <p>
-                    Outside of tech, I am interested in Psychology, Philosophy, Language,
-                    History, and Storytelling. When I'm not working on programming projects,
-                    I enjoy watching Formula 1, playing video games, and learning German.
-                </p>
+                <div className='text'>
+                    <h1>Bio</h1>
+                    <p>
+                        Hi, my name is Ben and I'm a Computer Science student at Oxford Brookes.
+                        I love bringing ideas and designs to life through problem solving and
+                        code, and am keen to pursuit a career in Desktop-, Web-, Mobile-,
+                        or Game Development after I graduate.
+                    </p>
+                    <p>
+                        Outside of tech, I am interested in Psychology, Philosophy, Language,
+                        History, and Storytelling. When I'm not working on programming projects,
+                        I enjoy watching Formula 1, playing video games, and learning German.
+                    </p>
+                </div>
                 <img
                     src="src/assets/hillpicture.jpg"
                     alt="image of Ben sat on a hill with his dog next to him"
                 />
             </section>
             <section className='skills'>
-                <h1>Skills</h1>
-                <section>
-                    <h2>Advanced</h2>
-                    <p>Python, Fuze4NS</p>
-                    <p>HTML</p>
-                    <h2>Confident</h2>
-                    <p>JavaScript, TypeScript, Java, C++</p>
-                    <p>VSCode, Git, GitHub, React, CSS</p>
-                    <h2>Knowledgeable</h2>
-                    <p>C#, Dart, Rust, C, Go</p>
-                    <p>SCSS, Flutter, SQL, Vue.js</p>
-                    <h2>Beginner</h2>
-                    <p>Ruby, Kotlin, Lua, Swift</p>
-                    <p>Tauri</p>
-                    <h2>Soft Skills</h2>
-                    <p>Communication, Planning, Time Management</p>
-                </section>
+                <img
+                    src="src/assets/hillpicture.jpg"
+                    alt="image of Ben sat on a hill with his dog next to him"
+                />
+                <div>
+                    <h1>Skills</h1>
+                    <section>
+                        <h2>Advanced</h2>
+                        <p>Python, Fuze4NS</p>
+                        <p>HTML</p>
+                        <h2>Confident</h2>
+                        <p>JavaScript, TypeScript, Java, C++</p>
+                        <p>VSCode, Git, GitHub, React, CSS</p>
+                        <h2>Knowledgeable</h2>
+                        <p>C#, Dart, Rust, C, Go</p>
+                        <p>SCSS, Flutter, SQL, Vue.js</p>
+                        <h2>Beginner</h2>
+                        <p>Ruby, Kotlin, Lua, Swift</p>
+                        <p>Tauri</p>
+                        <h2>Soft Skills</h2>
+                        <p>Communication, Planning, Time Management</p>
+                    </section>
+                </div>
             </section>
             <section className='education'>
-                <h1>Education</h1>
-                <section>
-                    <h2>Degree</h2>
-                    <h3>BSc Computer Science at Oxford Brookes (2021-2025)</h3>
-                    <ul className='modules'></ul>
-                </section>
-                <section>
-                    <h2>A-Level</h2>
-                    <h3>Wheatley Park Sixth Form</h3>
-                    <ul className="subjects"></ul>
-                </section>
-                <p>See my full education background on <a href="https://www.linkedin.com/in/bentaylor25">LinkedIn</a></p>
+                <div>
+                    <h1>Education</h1>
+                    <section>
+                        <h2>Degree</h2>
+                        <h3>BSc Computer Science at Oxford Brookes (2021-2025)</h3>
+                        {/* <ul className='modules'>
+                            <li>Problem Solving and Programming - A+ (96%)</li>
+                            <li>Foundations of Computer Systems - A+ (77%)</li>
+                            <li>Basic Communications and PC Networking - A (70%)</li>
+                            <li>Information Systems - A+ (93%)</li>
+                            <li>Object Oriented Programming - A+ (98%)</li>
+                            <li>DevOps - A+ (84%)</li>
+                            <li>Mathematics for Computing - A+ (100%)</li>
+                            <li>Software Development in C and C++ - A+ (100%)</li>
+                            <li>Data Structures and Algorithms - A+ (100%)</li>
+                            <li>Foundations of Security - A (73%)</li>
+                            <li>Innovative Product Development - [Sep22-May23]</li>
+                            <li>Foundations of Computation - [Jan23-May23]</li>
+                            <li>Databases - [Jan23-May23]</li>
+                            <li>Web App Development - [Jan23-May23]</li>
+                        </ul> */}
+                        <table className='modules'>
+                            <tr>
+                                <th style={{width: "80%"}}>Module Name</th>
+                                <th style={{width: "20%"}}>Result</th>
+                            </tr>
+                            <tr>
+                                <td>Problem Solving and Programming</td>
+                                <td>A+ (96%)</td>
+                            </tr>
+                            <tr>
+                                <td>Foundations of Computer Systems</td>
+                                <td>A+ (77%)</td>
+                            </tr>
+                            <tr>
+                                <td>Basic Communications and PC Networking</td>
+                                <td>A (70%)</td>
+                            </tr>
+                            <tr>
+                                <td>Information Systems</td>
+                                <td>A+ (93%)</td>
+                            </tr>
+                            <tr>
+                                <td>Object Oriented Programming</td>
+                                <td>A+ (98%)</td>
+                            </tr>
+                            <tr>
+                                <td>DevOps</td>
+                                <td>A+ (84%)</td>
+                            </tr>
+                            <tr>
+                                <td>Mathematics for Computing</td>
+                                <td>A+ (100%)</td>
+                            </tr>
+                            <tr>
+                                <td>Software Development in C and C++</td>
+                                <td>A+ (100%)</td>
+                            </tr>
+                            <tr>
+                                <td>Data Structures and Algorithms</td>
+                                <td>A+ (100%)</td>
+                            </tr>
+                            <tr>
+                                <td>Foundations of Security</td>
+                                <td>A (73%)</td>
+                            </tr>
+                            <tr>
+                                <td>Innovative Product Development</td>
+                                <td>[Sep22-May22]</td>
+                            </tr>
+                            <tr>
+                                <td>Foundations of Computation</td>
+                                <td>[Jan23-May23]</td>
+                            </tr>
+                            <tr>
+                                <td>Databases</td>
+                                <td>[Jan23-May23]</td>
+                            </tr>
+                            <tr>
+                                <td>Web App Development</td>
+                                <td>[Jan23-May23]</td>
+                            </tr>
+                        </table>
+                    </section>
+                    <p>See my full education background on <a href="https://www.linkedin.com/in/bentaylor25">LinkedIn</a></p>
+                </div>
+                <img
+                    src="src/assets/hillpicture.jpg"
+                    alt="image of Ben sat on a hill with his dog next to him"
+                />
             </section>
             <section className='other'>
-                <h1>Other</h1>
-                <section>
-                    <h2>Student Rep</h2>
-                    <p></p>
-                </section>
+                <img
+                    src="src/assets/hillpicture.jpg"
+                    alt="image of Ben sat on a hill with his dog next to him"
+                />
+                <div>
+                    <h1>Other</h1>
+                    <section>
+                        <h2>Student Rep</h2>
+                        <p></p>
+                    </section>
+                </div>
             </section>
         </main>
     )
