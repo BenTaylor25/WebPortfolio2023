191edf6e7daea5309f2dac1ebd11479af0055f42
Ben Taylor <ben.taylor2573@gmail.com>
2023-02-05 16:31:25 +0000

    added PAL to work

--- web-portfolio-2023/src/components/pages/Work.tsx
+++ web-portfolio-2023/src/components/pages/Work.tsx
@@ -19,6 +19,34 @@ export default function Work() {
             </section>
 
             <section className="entry">
+                <a className="image" href="https://brookes.ac.uk">
+                    <img src="src/assets/work/obu.png" alt="Oxford Brookes University Logo" />
+                </a>
+                <div className="text">
+                    <h1>PAL Lead Tutor - Oxford Brookes University</h1>
+                    <h2>Jul 2022 - present</h2>
+                    <p>
+                        Having performed well in the first year of <a href="https://brookes.ac.uk">University</a>,
+                        I was offered a role helping the 2022-2023 first-year students, leading the Peer-Assisted 
+                        Learning (PAL) scheme.
+                    </p>
+                    <p>
+                        I set up a community Discord server complete with relevant and organised resources,
+                        a ticketing system for private help requests, and a well-organised channel list.
+                        I am leading a team of second-year students in delivering academic and logistical help.
+                    </p>
+                    <p>
+                        I was required to give a short speech in front of approximately 120 first-year students
+                        to introduce the PAL scheme. Many of the students were struggling with a module exclusive
+                        to the Artificial Intelligence course. Where none of us PAL students studied this course,
+                        we were unable to offer direct help. I took the initiative and, both: informed the module
+                        leader, and encouraged the students to speak up. As a result, over half of the AI students
+                        made use of the module leader's drop-in hours, which are usually neglected.
+                    </p>
+                </div>
+            </section>
+
+            <section className="entry">
                 <a className="image" href="https://digivue.co.uk">
                     <img src="src/assets/work/digivue.jpg" alt="DigiVue Logo" />
                 </a>
