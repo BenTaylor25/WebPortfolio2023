2a49c07e01c4ac0d8eeefc32fd0d5d45b6842890
Ben Taylor <ben.taylor2573@gmail.com>
2023-02-05 15:59:20 +0000

    freelance web dev work

--- web-portfolio-2023/src/components/pages/Work.tsx
+++ web-portfolio-2023/src/components/pages/Work.tsx
@@ -1,8 +1,21 @@
+import './Work.scss';
 
 export default function Work() {
     return (
         <main className="work">
-            <h1>work</h1>
+            <section className="entry">
+                <img src="src/assets/icon.jpg" alt="Ben Icon" />
+                <div className="text">
+                    <h1>Freelance Web Development</h1>
+                    <p>
+                        I designed, developed, and continue to maintain the
+                        website for <a href="https://decorareltd.co.uk">Decorare</a>.
+                        The reputable Decorare team are experienced in delivering
+                        high-quality interior and exterior decorating as well as
+                        landscaping and garden maintenance.
+                    </p>
+                </div>
+            </section>
         </main>
     )
 }
