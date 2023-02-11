c6bc57f72fa093168cfa59b5fda50e2559362e1c
Ben Taylor <ben.taylor2573@gmail.com>
2023-02-11 09:31:18 +0000

    project entry basic structure

--- web-portfolio-2023/src/components/Projects/ProjectEntry.tsx
+++ web-portfolio-2023/src/components/Projects/ProjectEntry.tsx
@@ -1,7 +1,7 @@
 
 interface Project {
     name: string,
-    description?: string,
+    description: string,
     imageURL?: string,
     githubURL?: string,
     youtubeURL?: string
@@ -11,8 +11,13 @@ export default function ProjectEntry(props: Project) {
 
     return (
         <section id="project-entry">
-            <h1>{ props.name }</h1>
-            <p>{ props.description }</p>
+            <div id="info">
+                <h1>{ props.name }</h1>
+                <p>{ props.description }</p>
+            </div>
+            <div id="image">
+                <img src={ props.imageURL } alt={ props.name } />
+            </div>
         </section>
     )
 }
