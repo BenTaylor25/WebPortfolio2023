4f983379f26cdb725b2d7724086e1d804b1fe987
Ben Taylor <ben.taylor2573@gmail.com>
2023-02-12 11:30:08 +0000

    added technologies to home page

--- web-portfolio-2023/src/components/pages/Home.tsx
+++ web-portfolio-2023/src/components/pages/Home.tsx
@@ -8,6 +8,8 @@ export default function Home() {
             <p>Computer Science Student;</p>
             <p>Aspiring Software Engineer.</p>
             <img src="src/assets/portrait.jpg" alt="portrait of Ben" />
+            <p>Python, Java, C++, TypeScript,</p>
+            <p>VSCode, Git, GitHub, React.</p>
         </main>
     );
 }
