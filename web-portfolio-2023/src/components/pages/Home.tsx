488057e1ff21c46aedfe7dc6da619848e3e0b95b
Ben Taylor <ben.taylor2573@gmail.com>
2023-03-02 23:46:54 +0000

    moved assets to public

--- web-portfolio-2023/src/components/pages/Home.tsx
+++ web-portfolio-2023/src/components/pages/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
             <h1>Ben Taylor</h1>
             <p>Computer Science Student;</p>
             <p>Aspiring Software Engineer.</p>
-            <img src="src/assets/portrait.jpg" alt="portrait of Ben" />
+            <img src={"public/assets/portrait.jpg"} alt="portrait of Ben" />
             <p>Python, Java, C++, TypeScript,</p>
             <p>VSCode, Git, GitHub, React.</p>
         </main>
