b9320ba6036b889050904cdddb389a886c524819
Ben Taylor <ben.taylor2573@gmail.com>
2023-07-11 20:36:52 +0100

    fitness page bio and images

--- web-portfolio-2023/src/components/pages/About/Fitness.tsx
+++ web-portfolio-2023/src/components/pages/About/Fitness.tsx
@@ -3,7 +3,27 @@ import './Fitness.scss';
 export default function Fitness() {
     return (
         <main className="Fitness">
-            Fitness
+            <h1>Fitness</h1>
+            <p>
+                Fitness isn't a great passion of mine, but it is
+                something that I care about. I was always a big
+                kid growing up, but by January 2021 it was really
+                bothering me and I decided to make a change.
+            </p>
+            <section id="transformation-gallery">
+                <img
+                    src={"assets/fitness/side-transformation.png"}
+                    alt="side-on comparison of me in January 2021 (113kg) and July 2023 (73kg)"
+                />
+                <img
+                    src={"assets/fitness/front-transformation.png"}
+                    alt="front-facing comparison of me in January 2021 (113kg) and July 2023 (73kg)"
+                />
+                <img
+                    src={"assets/fitness/back-transformation.png"}
+                    alt="back-facing comparison of me in January 2021 (113kg) and July 2023 (73kg)"
+                />
+            </section>
         </main>
     );
 }
