53e752f7d808f0fac6cf4a9ce0dafd678a4e3520
Ben Taylor <ben.taylor2573@gmail.com>
2023-08-06 22:28:28 +0100

    updated home page

--- web-portfolio-2023/src/components/pages/Home.tsx
+++ web-portfolio-2023/src/components/pages/Home.tsx
@@ -5,11 +5,11 @@ export default function Home() {
     return (
         <main className='home'>
             <h1>Ben Taylor</h1>
-            <p>Computer Science Student;</p>
-            <p>Aspiring Software Engineer.</p>
+            <h2>Computer Science Student,</h2>
+            <h2>Intern Software Engineer at Scuderia AlphaTauri.</h2>
             <img src={"assets/portrait.jpg"} alt="portrait of Ben" />
-            <p>Python, TypeScript, C#</p>
-            <p>VSCode, Git, React.</p>
+            <p>Python, TypeScript, C#,</p>
+            <p>React, Git, VSCode.</p>
         </main>
     );
 }
