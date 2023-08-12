3f06e225654f5ec3a10b18276947e6cfdb762515
benlaptop25 <ben.taylor2573@gmail.com>
2023-08-12 11:38:20 +0100

    added link to Scuderia AlphaTauri

--- web-portfolio-2023/src/components/pages/Work.tsx
+++ web-portfolio-2023/src/components/pages/Work.tsx
@@ -11,7 +11,8 @@ export default function Work() {
                     <h2>Jul 2023 - present</h2>
                     <p>
                         For my University placement year, I am a Software Engineer in
-                        the Aerodynamics department of AlphaTauri Formula 1 team.
+                        the Aerodynamics department of <a href="https://scuderia.alphatauri.com/en/">
+                        AlphaTauri Formula 1 team</a>.
                         I am developing internal tools for various aero teams to
                         facilitate improvements to the performance of the cars.
                     </p>
