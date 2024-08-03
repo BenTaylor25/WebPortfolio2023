e134a4d378f05960fa920fdfb171834f4bc91120
Ben Taylor <ben.taylor2573@gmail.com>
2024-08-03 22:39:41 +0100

    updated job role and image on home page

--- web-portfolio-2023/src/components/pages/Home.tsx
+++ web-portfolio-2023/src/components/pages/Home.tsx
@@ -6,9 +6,9 @@ export default function Home() {
         <main className='home'>
             <h1>Ben Taylor</h1>
             <h2>Computer Science Student,</h2>
-            <h2>Intern Software Engineer at Scuderia AlphaTauri.</h2>
-            <img src={"assets/portrait.jpg"} alt="portrait of Ben" />
-            <p>Python, TypeScript, C#,</p>
+            <h2>Former Intern Software Engineer at Scuderia AlphaTauri.</h2>
+            <img src={"assets/windtunnel.jpg"} alt="portrait of Ben" />
+            <p>C#, TypeScript, Python</p>
             <p>React, Git, VSCode.</p>
         </main>
     );
